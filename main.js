3b319ac8c65380706116ddb426db6b3a61b61e24
DennisWilson <tulawilson@Tulas-MacBook-Air.local>
2018-02-13 20:17:11 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,167 @@
+//promise to call url ==================================================
+const vanillaGet = url => {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.addEventListener('load', e => {
+			const data = JSON.parse(e.currentTarget.response);
+			resolve(data)
+		})
+		xhr.addEventListener('error', err => {
+			reject(err);
+		})
+		xhr.send();
+	});
+}
+// state for the memes//==================================================
+const state = {
+    searchMeme:'',
+    validSearchMeme: false,
+    errMessage: '',
+    memes: [],
+    setSearchMemes: (meme) => {
+        if (meme === '' || meme.trim() === "") {
+            state.validSearchMeme = false;
+            state.errMessage = "search cannot be empty";
+            return;
+        }
+        if (meme.length < 3) {
+            state.validSearchMeme = false;
+            state.errMessage = "Query must be at least three chars long";
+            return;
+        }
+        state.validSearchMeme = true;
+        state.errMessage = "";
+        state.searchMeme = meme;
+    },
+
+    refreshMemes: (memes) => {
+        if (state.validSearchMeme = false) {
+            return;
+        }
+        const apiEndPoint = `http://api.giphy.com/v1/gifs/search?api_key=syP7w6LxYPKIJiQ8sP7lH5X7PsMqqAhC&q=${state.searchMeme}`
+            const start = Date.now();
+        return vanillaGet(apiEndPoint).then(data => {
+            state.memes = [];
+            for (let i = 0; i < data.data.length; i++) {
+                const memeUrl = data.data[i].images.downsized_medium.url;
+
+                state.memes.push(memeUrl)
+            }
+            return true;
+
+        })
+        .catch(e=>{
+        });
+    }
+};
+
+
+// ================================================================
+//set variables for the search bars, buttons and container//
+const docOne = document.querySelector.bind(document);
+const inPuMemes = docOne('.js-input-memes');
+const srchBtn = docOne('.js-search-btn');
+const randBtn = docOne('.js-random-btn');
+const funnyBtn = docOne('.js-funny-btn');
+const catsBtn = docOne('.js-cats-btn');
+const failBtn = docOne('.js-fails-btn');
+const memesCon = docOne('.js-memesCont');
+const hint = docOne('.js-hint');
+
+// ================================================================
+//render methode///
+const render=()=>{
+  if (state.isValidSearchQuery === false) {
+		hint.innerHTML = state.errMessage;
+		hint.style.color = 'red';
+		hint.style.width = '100%';
+		hint.style.display = 'block';
+		return;
+	}
+  hint.innerHTML = '';
+	memesCon.innerHTML = `<div id="loading"></div>`;
+	srchBtn.setAttribute('disabled', 'disabled');
+	inPuMemes.setAttribute('disabled', 'disabled');
+    state.refreshMemes(inPuMemes.value).then(()=>{
+     srchBtn.removeAttribute('disabled');
+     inPuMemes.removeAttribute('disabled');
+     inPuMemes.value = '';
+     inPuMemes.setAttribute('placeholder', state.searchMeme);
+     let str = '';
+     for (let i = 0; i<state.memes.length; i++){
+       str += `<img src=${state.memes[i]}>`
+     }
+     memesCon.innerHTML = str;
+   })
+
+}
+
+
+
+// ================================================================
+// event handlers
+const udateStateAndRedraw = (callback) => {
+    callback();
+    render();
+}
+
+const onKeyPressed = e =>{
+  if(e.keyCode === 13){
+      state.setSearchMemes(e.target.value);
+    udateStateAndRedraw(()=>{
+      state.setSearchMemes(e.target.value);
+
+    });
+  }
+}
+inPuMemes.addEventListener('keypress', onKeyPressed);
+
+srchBtn.addEventListener('click', e => {
+  const valu = inPuMemes.value;
+udateStateAndRedraw(()=>{
+    state.setSearchMemes(valu);
+  });
+});
+
+
+randBtn.addEventListener('click', e => {
+udateStateAndRedraw(()=>{
+    state.searchMeme('random');
+  });
+});
+
+
+funnyBtn.addEventListener('click', e => {
+udateStateAndRedraw(()=>{
+    state.searchMeme();
+    state.refreshMemes()
+  });
+});
+
+
+catsBtn.addEventListener('click', e => {
+udateStateAndRedraw(()=>{
+    state.searchMeme();
+    state.refreshMemes()
+  });
+});
+
+
+failBtn.addEventListener('click', e => {
+udateStateAndRedraw(()=>{
+    state.searchMeme('fails');
+
+    state.refreshMemes()
+  });
+});
+
+
+
+
+
+
+
+
+
+//
